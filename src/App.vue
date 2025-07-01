6dca16774cc38e3ad0a4a68addf5207ca91ae28c
griga2 <griga2999wow@gmail.com>
2025-07-02 01:00:06 +0700

    awd

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+onMounted(() => {
+  router.push('/main')
+})
 </script>
 
 <template>
